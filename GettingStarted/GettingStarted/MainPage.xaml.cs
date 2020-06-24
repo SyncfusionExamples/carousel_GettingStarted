b3b11d6859e185016caf6ad2a6e14a5ab56b860f
AzureAD\RamyaSoundarRajan <ramya.soundarrajan@syncfusion.com>
2020-06-24 19:42:50 +0530

    Added sample.

---
+++ GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new CarouselViewModel();
+        }
+    }
+    public class CarouselViewModel
+    {
+        public CarouselViewModel()
+        {
+            ImageCollection.Add(new CarouselModel("image1.png"));
+            ImageCollection.Add(new CarouselModel("image2.png"));
+            ImageCollection.Add(new CarouselModel("image3.png"));
+            ImageCollection.Add(new CarouselModel("image4.png"));
+            ImageCollection.Add(new CarouselModel("image5.png"));
+        }
+        private List<CarouselModel> imageCollection = new List<CarouselModel>();
+        public List<CarouselModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+    }
+}
